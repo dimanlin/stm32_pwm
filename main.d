283770354a77ddcacbf843550f4d06702bfdb986
Dmitry khromov <devandart@ya.ru>
2015-01-27 22:41:09 +0400

    first commit

---
+++ main.d
@@ -0,0 +1,7 @@
+.\main.o: main.c
+.\main.o: C:\Keil2\ARM\PACK\Keil\STM32F1xx_DFP\1.0.4\Device\Include\stm32f10x.h
+.\main.o: C:\Keil2\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cm3.h
+.\main.o: C:\Keil2\ARM\ARMCC\bin\..\include\stdint.h
+.\main.o: C:\Keil2\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cmInstr.h
+.\main.o: C:\Keil2\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cmFunc.h
+.\main.o: C:\Keil2\ARM\PACK\Keil\STM32F1xx_DFP\1.0.4\Device\Include\system_stm32f10x.h
