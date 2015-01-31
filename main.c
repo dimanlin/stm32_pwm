9ef0a2b7a52d6726e4b34ea1598417084a31de51
Dmitry khromov <devandart@ya.ru>
2015-02-01 00:56:02 +0400

    fix

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "stm32f10x.h"
 
+unsigned short int per = 500;
+int up_dw = 1;
+
 int main() {
   RCC->APB2ENR |= (RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN);
   GPIOA->CRL |= GPIO_CRL_MODE1;
@@ -11,8 +14,17 @@ int main() {
   TIM2->CCMR1 |= (TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1);//OC2M = 110 - PWM mode 1
 	TIM2->CCER |= TIM_CCER_CC2E;
   TIM2->ARR = 1000 - 1;
-  TIM2->CCR2 = 500;
+  TIM2->CCR2 = 100;
+	TIM2->DIER |= TIM_DIER_UIE;
+	NVIC_EnableIRQ(TIM2_IRQn);
   TIM2->CR1 |= TIM_CR1_CEN;
 
   while(1);
+}
+
+void TIM2_IRQHandler() 
+{
+	TIM2->SR&=~TIM_SR_UIF;
+	//TIM2->CCER |= TIM_CCER_CC2P;
+	
 }
